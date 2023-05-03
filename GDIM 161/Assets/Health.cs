987120190a24ca18ccdc7d9c5af0a05190da9ff2
hungbuiwork <hungbuiwork@gmail.com>
2023-05-02 18:01:14 -0700

    Health script working

---
+++ GDIM 161/Assets/Health.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    //created by Hung Bui
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth;
+
+    public Action Died; //subscribe functions upon death
+    public Action<float> Damaged; //subscribe functions upon taking damage
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        Died += () => Destroy(this.gameObject); //currently, destroys the game object this is attached to. Delete this line later.
+    }
+
+    public void Damage(float amount)
+    {
+        currentHealth -= amount;
+        CheckHealth();
+        Damaged(amount);
+    }
+    
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        if (amount > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public float getHealth()
+    {
+        return currentHealth;
+    }
+    private float CheckHealth()
+    {
+        if (currentHealth < 0) { Died(); }
+        return currentHealth;
+    }
+
+
+
+}
