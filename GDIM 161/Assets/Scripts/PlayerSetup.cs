59f1ac2693f8baf0f88fed011dd46b40761001a6
zanemoore <zanemoore00@gmail.com>
2023-05-16 17:31:16 -0700

    player health fix? (part 2)

--- GDIM 161/Assets/Scripts/PlayerSetup.cs
+++ GDIM 161/Assets/Scripts/PlayerSetup.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerSetup : MonoBehaviour
 {
     [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private PlayerMovement playerHealth;
+    [SerializeField] private PlayerHealth playerHealth;
     [SerializeField] private ProjectileLauncher projectileLauncher;
     [SerializeField] private CharacterController playerController;
     [SerializeField] private GameObject playerCamera;
