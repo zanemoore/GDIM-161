ba74df03df97729b5a999b6bbee5b1452cd324f1
hungbuiwork <hungbuiwork@gmail.com>
2023-05-25 16:37:23 -0700

    zombie hit visual feedback

---
+++ GDIM 161/Assets/ZombieVFX.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieVFX : MonoBehaviour
+{
+    [SerializeField] private Material damageMaterial;
+    [SerializeField] private Material actualMaterial;
+    [SerializeField] private SkinnedMeshRenderer skin;
+
+
+    private void Awake()
+    {
+        actualMaterial = skin.material;
+        this.GetComponent<ZombieHealth>().Damaged += IndicateDamage;
+    }
+
+    public void IndicateDamage(float damage)
+    {
+        skin.material = damageMaterial;
+        Invoke("ResetSkin", 0.3f);
+    }
+
+    public void ResetSkin()
+    {
+        skin.material = actualMaterial;
+    }
+}
