867670adb525cc09e2e028264c19106ce7cd77c1
Diego <73369316+dnajerab@users.noreply.github.com>
2023-06-01 15:40:27 -0700

    Help

--- GDIM 161/Assets/Scripts/WaveInitiator.cs
+++ GDIM 161/Assets/Scripts/WaveInitiator.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+// DO NOT LOOK AT THIS CODE DO NOT LOOK AT THIS CODE DO NOT LOOK AT THIS CODE 
+// DO NOT - DIEGO
 public class WaveInitiator : MonoBehaviour
 {
     [SerializeField] private GameObject _waveManagerPrefab;
     [SerializeField] private float _minDistanceToInteract;
-    [SerializeField] private TextMeshProUGUI _objectiveInstructions;
-    [SerializeField] private TextMeshProUGUI _allPlayersInZoneReminder;
 
     private WaveManager _waveManager;
     private int _numPlayersNeeded;
@@ -25,38 +25,114 @@ public class WaveInitiator : MonoBehaviour
     {
         if (_playersInZone.Count > 0)
         {
-            _objectiveInstructions.gameObject.SetActive(true);
+            foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                foreach (Transform child in player.transform)
+                {
+                    if (child.gameObject.name == "Player Canvas")
+                    {
+                        GameObject canvas = child.gameObject;
+
+                        foreach (Transform canvasChild in canvas.transform)
+                        {
+                            if (canvasChild.gameObject.name == "WaveInitiatorObjectiveInstructions")
+                            {
+                                canvasChild.gameObject.SetActive(true);
+                            }
+                        }
+
+                    }
+                }
+            }
         }
         else
         {
-            _objectiveInstructions.gameObject.SetActive(false);
-            _allPlayersInZoneReminder.gameObject.SetActive(false);
+            foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                foreach (Transform child in player.transform)
+                {
+                    if (child.gameObject.name == "Player Canvas")
+                    {
+                        GameObject canvas = child.gameObject;
+
+                        foreach (Transform canvasChild in canvas.transform)
+                        {
+                            if (canvasChild.gameObject.name == "WaveInitiatorObjectiveInstructions")
+                            {
+                                canvasChild.gameObject.SetActive(false);
+                            }
+                            if (canvasChild.gameObject.name == "AllPlayersInZoneReminder")
+                            {
+                                canvasChild.gameObject.SetActive(false);
+                            }
+                        }
+
+                    }
+                }
+            }
         }
 
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, _minDistanceToInteract))
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
         {
-            if (hit.collider.gameObject.tag == "WaveInitiator")// && Input.GetKeyDown(KeyCode.F))
+            Camera cam = player.GetComponent<Camera>();
+
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, _minDistanceToInteract))
             {
-                foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+                if (hit.collider.gameObject.tag == "WaveInitiator" && Input.GetKeyDown(KeyCode.F))
                 {
-                    Debug.Log("Here");
-                    if (Input.GetKeyDown(KeyCode.F))
+                    if (IsAllPlayersInZone())
                     {
-                        if (IsAllPlayersInZone())
+                        foreach (GameObject player1 in GameObject.FindGameObjectsWithTag("Player"))
                         {
-                            _objectiveInstructions.gameObject.SetActive(false);
-                            _allPlayersInZoneReminder.gameObject.SetActive(false);
+                            foreach (Transform child in player1.transform)
+                            {
+                                if (child.gameObject.name == "Player Canvas")
+                                {
+                                    GameObject canvas = child.gameObject;
 
-                            _waveManager.SetUp();
+                                    foreach (Transform canvasChild in canvas.transform)
+                                    {
+                                        if (canvasChild.gameObject.name == "WaveInitiatorObjectiveInstructions")
+                                        {
+                                            canvasChild.gameObject.SetActive(false);
+                                        }
+                                        if (canvasChild.gameObject.name == "AllPlayersInZoneReminder")
+                                        {
+                                            canvasChild.gameObject.SetActive(false);
+                                        }
+                                    }
 
-                            Destroy(this);
+                                }
+                            }
                         }
-                        else
+
+                        _waveManager.SetUp();
+
+                        Destroy(this);
+                    }
+                    else
+                    {
+                        foreach (GameObject player1 in GameObject.FindGameObjectsWithTag("Player"))
                         {
-                            _allPlayersInZoneReminder.gameObject.SetActive(true);
+                            foreach (Transform child in player1.transform)
+                            {
+                                if (child.gameObject.name == "Player Canvas")
+                                {
+                                    GameObject canvas = child.gameObject;
+
+                                    foreach (Transform canvasChild in canvas.transform)
+                                    {
+                                        if (canvasChild.gameObject.name == "AllPlayersInZoneReminder")
+                                        {
+                                            canvasChild.gameObject.SetActive(true);
+                                        }
+                                    }
+
+                                }
+                            }
                         }
                     }
                 }
