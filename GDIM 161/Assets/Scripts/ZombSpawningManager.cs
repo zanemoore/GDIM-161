0f26426e34be664caea673c06a3787b1874aed2a
Diego <73369316+dnajerab@users.noreply.github.com>
2023-06-16 21:41:15 -0700

    ZombSpawningManager should work in photon

--- GDIM 161/Assets/Scripts/ZombSpawningManager.cs
+++ GDIM 161/Assets/Scripts/ZombSpawningManager.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,7 +29,7 @@ public class ZombSpawningManager : MonoBehaviour
                 spawner.Spawn();
             }
 
-            Destroy(this.gameObject);
+            PhotonNetwork.Destroy(this.gameObject);
         }
     }
 }
