d9c04282698c804880d84ccdcbb9f4bd586d68c8
Diego <73369316+dnajerab@users.noreply.github.com>
2023-06-16 20:00:02 -0700

    ZombSpawningManager script + prefab

---
+++ GDIM 161/Assets/Scripts/ZombSpawningManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombSpawningManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _spawnersAhead;
+    [SerializeField] private List<GameObject> _spawnersBehind;
+    [SerializeField] private Transform _behindZombiesDest;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ZombieSpawner spawner;
+
+            foreach (GameObject spawnerPrefabAhead in _spawnersAhead)
+            {
+                spawner = spawnerPrefabAhead.GetComponent<ZombieSpawner>();
+                spawner.Spawn();
+            }
+
+            foreach (GameObject spawnerPrefabBehind in _spawnersBehind)
+            {
+                spawner = spawnerPrefabBehind.GetComponent<ZombieSpawner>();
+                spawner.SetZombiesDestination(_behindZombiesDest);
+                spawner.Spawn();
+
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
